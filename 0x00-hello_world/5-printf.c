23578586c5b21ac3ea8d269d027145adea05db45
Larysmith <ayinla.olanrewaju@gmail.com>
2022-06-30 22:51:57 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: 'Using printf'
  *
- *   *
- *
- *    * Description: 'Using printf'
- *
- *     *
- *
- *      * Return: Always zero
- *
- *       */
-
-
+ * Return: Always zero
+ */
 
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-				return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
