610446844bc0a54b8a7f92b5daeedd2c8970e6d1
Larysmith <ayinla.olanrewaju@gmail.com>
2022-06-30 14:41:28 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: 'C programming'
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		char mystr[] = "\"Programming is like building a multilingual puzzle";
+
+
+
+			puts(mystr);
+
+					return (0);
+
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+readmefor c programming 
