e1da79bb0f64fddaa15dd025cbd6e83389636d7e
Larysmith <ayinla.olanrewaju@gmail.com>
2022-06-30 22:50:10 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   * Description: 'C programming'
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * main - Entry point
+ * Description: 'C programming'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
 
-		char mystr[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(mystr);
-
-					return (0);
-
+	puts(mystr);
+		return (0);
 }
